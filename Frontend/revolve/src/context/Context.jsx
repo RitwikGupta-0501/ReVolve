9c08c0945219e41a24302e8a3bea6e2344e78c7f
Kanav Dhanda <70010606+kanavdhanda@users.noreply.github.com>
2024-04-07 05:09:53 +0530

    help

--- Frontend/revolve/src/context/Context.jsx
+++ Frontend/revolve/src/context/Context.jsx
@@ -32,11 +32,11 @@ const ContextProvider = (props) => {
         let inputToSendM;
         if (prompt !== undefined) {
             // If prompt is provided, use it with prefix
-            inputToSendM = "i am sending you a text after a colon. mention the materials talked. in your response only mention the name of the material i mentioned in that text: " + prompt;
+            inputToSendM = "i am sending you a text after a colon. mention the materials talked like for example Aluminum, copper, steel, plastics, wood, concrete, glass, ceramincs, etc . in your response only mention the name of the material i mentioned in that text: " + prompt;
             setRecentPrompt(prompt);
         } else {
             // If prompt is not provided, use user input with prefix
-            inputToSendM = "i am sending you a text after a colon. mention the materials talked. in your response only mention the name of the material i mentioned in that text:  " + input;
+            inputToSendM = "i am sending you a text after a colon. mention the materials talked like for example Aluminum, copper, steel, plastics, wood, concrete, glass, ceramincs, etc . in your response only mention the name of the material i mentioned in that text: " + input;
             setPrevPrompts(prev => [...prev, input]);
             setRecentPrompt(input);
         }
@@ -50,11 +50,11 @@ const ContextProvider = (props) => {
         let inputToSendG;
         if (prompt !== undefined) {
             // If prompt is provided, use it with prefix
-            inputToSendG = "i am sending you a text after a colon. mention the materials talked. in your response only mention the name of the material i mentioned in that text: " + prompt;
+            inputToSendG = "i am sending you a text after a colon. mention the name(exact alphanumeric value as mentioned in the text or received data, the grade should not be changed) of the material grade (which is the composition of which it is made of like for steel it can be AISI 1018). i dont want the product descriptions or dimensions to be shown : " + prompt;
             setRecentPrompt(prompt);
         } else {
             // If prompt is not provided, use user input with prefix
-            inputToSendG = "Your Prefix String " + input;
+            inputToSendG = "i am sending you a text after a colon. mention the name(exact alphanumeric value as mentioned in the text or received data, the grade should not be changed) of the material grade (which is the composition of which it is made of like for steel it can be AISI 1018). i dont want the product descriptions or dimensions to be shown : " + input;
             setPrevPrompts(prev => [...prev, input]);
             setRecentPrompt(input);
         }
@@ -63,24 +63,24 @@ const ContextProvider = (props) => {
         console.log(responseG);
 
             //  Rest of your code remains unchanged..
-        let responseArray = responseM.split('**');
-        let newArray = "";
-        for (let i = 0; i < responseArray.length; i++) {
-            if (i === 0 || i % 2 !== 1) {
-                newArray += responseArray[i]
-            }
-            else {
-                newArray += "<b>" + responseArray[i] + "</b>"
-            }
-        }
-        console.log(newArray);
-        responseArray = newArray.split('*').join("</br>").split(" ");
-        for (let i = 0; i < responseArray.length; i++) {
-            const nextWord = responseArray[i];
-            delayPara(i, nextWord + " ")
-        }
-        setLoading(false);
-        setInput("")
+        // let responseArray = responseM.split('**');
+        // let newArray = "";
+        // for (let i = 0; i < responseArray.length; i++) {
+        //     if (i === 0 || i % 2 !== 1) {
+        //         newArray += responseArray[i]
+        //     }
+        //     else {
+        //         newArray += "<b>" + responseArray[i] + "</b>"
+        //     }
+        // }
+        // console.log(newArray);
+        // responseArray = newArray.split('*').join("</br>").split(" ");
+        // for (let i = 0; i < responseArray.length; i++) {
+        //     const nextWord = responseArray[i];
+        //     delayPara(i, nextWord + " ")
+        // }
+        // setLoading(false);
+        // setInput("")
 
         const reqs = { 
             grade : responseG,
@@ -91,6 +91,7 @@ const ContextProvider = (props) => {
         console.log(res);
         console.log(res.data);
       })
+
       let fres;
       axios.post('/api/get_objects/') 
       .then(res => {
