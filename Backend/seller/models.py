3cb6340b1e2c7c556a78aa189dde2519afb03059
Shreyas Mahajan <142911768+ShreyasMahajann@users.noreply.github.com>
2024-04-06 20:44:08 +0000

    correct seller

--- Backend/seller/models.py
+++ Backend/seller/models.py
@@ -11,15 +11,9 @@ class seller(models.Model):
     seller_name=models.CharField(max_length=10)
     seller_location=models.CharField(max_length=50)
     seller_rating=models.IntegerField(default=0,validators=[MinValueValidator(0),MaxValueValidator(5)])
+    product_name = models.CharField(max_length=100,blank=True)
+    product_description=models.TextField(blank=True)
+    
     def __str__(self)->str:
         return self.seller_name
-    
-class Product(models.Model):
-    seller = models.ForeignKey(seller, on_delete=models.CASCADE, related_name="products")
-    product_name = models.CharField(max_length=100)
-    product_description=models.TextField()
-    
-    # Add other fields for product details such as price, description, etc.
-    def __str__(self):
-        return self.product_name
-    
+   
