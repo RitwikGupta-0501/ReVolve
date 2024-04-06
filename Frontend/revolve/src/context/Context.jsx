f9e18b5b59140968116b882c6159d79ef13670b7
Kanav Dhanda <70010606+kanavdhanda@users.noreply.github.com>
2024-04-07 04:24:18 +0530

    help

--- Frontend/revolve/src/context/Context.jsx
+++ Frontend/revolve/src/context/Context.jsx
@@ -1,5 +1,6 @@
 import { createContext, useState } from "react";
 import runChat from "../config/gemini";
+import axios from 'axios';
 
 export const Context = createContext();
 
@@ -22,25 +23,47 @@ const ContextProvider = (props) => {
         setResultData("");
         setLoading(true);
         setShowResult(true);
-        let response;
+
+        // for name of material
+
+        let responseM;
     
         // Add prefix string here
-        let inputToSend;
+        let inputToSendM;
         if (prompt !== undefined) {
             // If prompt is provided, use it with prefix
-            inputToSend = "i am sending you a text after a colon. mention the materials talked. in your response only mention the name of the material i mentioned in that text: " + prompt;
+            inputToSendM = "i am sending you a text after a colon. mention the materials talked. in your response only mention the name of the material i mentioned in that text: " + prompt;
             setRecentPrompt(prompt);
         } else {
             // If prompt is not provided, use user input with prefix
-            inputToSend = "Your Prefix String " + input;
+            inputToSendM = "i am sending you a text after a colon. mention the materials talked. in your response only mention the name of the material i mentioned in that text:  " + input;
+            setPrevPrompts(prev => [...prev, input]);
+            setRecentPrompt(input);
+        }
+         
+        responseM = await runChat(inputToSendM);
+        console.log(responseM);
+        // For grade of material
+        let responseG;
+    
+        // Add prefix string here
+        let inputToSendG;
+        if (prompt !== undefined) {
+            // If prompt is provided, use it with prefix
+            inputToSendG = "i am sending you a text after a colon. mention the materials talked. in your response only mention the name of the material i mentioned in that text: " + prompt;
+            setRecentPrompt(prompt);
+        } else {
+            // If prompt is not provided, use user input with prefix
+            inputToSendG = "Your Prefix String " + input;
             setPrevPrompts(prev => [...prev, input]);
             setRecentPrompt(input);
         }
     
-        response = await runChat(inputToSend);
-        console.log(response);
-        // Rest of your code remains unchanged..
-        let responseArray = response.split('**');
+        responseM = await runChat(inputToSendG);
+        console.log(responseG);
+
+            //  Rest of your code remains unchanged..
+        let responseArray = responseM.split('**');
         let newArray = "";
         for (let i = 0; i < responseArray.length; i++) {
             if (i === 0 || i % 2 !== 1) {
@@ -58,6 +81,47 @@ const ContextProvider = (props) => {
         }
         setLoading(false);
         setInput("")
+
+        const reqs = { 
+            grade : responseG,
+            material : responseM
+        }
+        axios.post(`https://jsonplaceholder.typicode.com/users`, { reqs })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+      let fres;
+      axios.post('/api/get_objects/') 
+      .then(res => {
+        let len = res.length;
+        for(int i=0)
+      })
+      .catch(error => {
+        console.error('Error fetching objects:', error);
+      });
+    
+
+
+        // Rest of your code remains unchanged..
+        // let responseArray = response.split('**');
+        // let newArray = "";
+        // for (let i = 0; i < responseArray.length; i++) {
+        //     if (i === 0 || i % 2 !== 1) {
+        //         newArray += responseArray[i]
+        //     }
+        //     else {
+        //         newArray += "<b>" + responseArray[i] + "</b>"
+        //     }
+        // }
+        // console.log(newArray);
+        // responseArray = newArray.split('*').join("</br>").split(" ");
+        // for (let i = 0; i < responseArray.length; i++) {
+        //     const nextWord = responseArray[i];
+        //     delayPara(i, nextWord + " ")
+        // }
+        // setLoading(false);
+        // setInput("")
     }
 
     const newChat = async () => {
