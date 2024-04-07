c1bb406d1d8147d8028da736e76176eb577d83c8
Kanav Dhanda <70010606+kanavdhanda@users.noreply.github.com>
2024-04-07 07:02:39 +0530

    run

---
+++ Frontend/revolve/src/App.jsx
@@ -0,0 +1,19 @@
+// import React, { useState } from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
+import Main from './components/Main/Main'
+// import MyComponent from './components/sendPrompt'
+
+const App = () => {
+
+
+  return (
+    <>
+     <Sidebar/>
+     <Main/>  
+      
+        {/* <MyComponent/>  */}
+    </>
+  )
+}
+
+export default App
